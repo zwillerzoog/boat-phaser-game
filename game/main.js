954be63799660aeca796d6448e4e45c2feb8bdaa
52lions06 <mckelvey.william@yahoo.com>
2017-10-09 16:18:00 -0400

    added a game folder to hold everthing

---
+++ game/main.js
@@ -0,0 +1,21 @@
+'use strict';
+let game = new Phaser.Game(800, 600, Phaser.AUTO, 'game'), Main = function () {};
+
+Main.prototype = {
+
+  preload: function () {
+    game.load.image('stars',    'assets/images/stars.jpg');
+    game.load.image('loading',  'assets/images/loading.png');
+    game.load.image('brand',    'assets/images/logo.png');
+    game.load.script('splash',  'states/Splash.js');
+  },
+
+  create: function () {
+    game.state.add('Splash', Splash);
+    game.state.start('Splash');
+  }
+
+};
+
+game.state.add('Main', Main);
+game.state.start('Main');
